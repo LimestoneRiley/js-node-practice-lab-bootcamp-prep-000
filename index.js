44280bedca4d4d8b7603715370dd04beed655c7b
Robert Riley <rob_a_riley@yahoo.com>
2018-09-07 17:19:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Your Name"
-var height = "Our Height"
+var height = "68"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) +1)
